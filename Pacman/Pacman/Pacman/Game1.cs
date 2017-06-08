08844441e7009cdffdeb6212b56e0b61c97bb918
FionaReziga <fiona.reziga@gmail.com>
2017-06-08 10:04:40 +0200

    Code

--- Pacman/Pacman/Pacman/Game1.cs
+++ Pacman/Pacman/Pacman/Game1.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using Pacman.Métier;
+using Pacman.Metier;
 using Pacman.IA;
 
 namespace Pacman
