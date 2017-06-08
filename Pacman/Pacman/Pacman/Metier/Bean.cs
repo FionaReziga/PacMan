3d1d1f05aefed260b345942ee71c9be304344383
FionaReziga <fiona.reziga@gmail.com>
2017-06-08 10:03:21 +0200

    Code clean

--- Pacman/Pacman/Pacman/Metier/Bean.cs
+++ Pacman/Pacman/Pacman/Metier/Bean.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Pacman.Métier
+namespace Pacman.Metier
 {
-    class Map
+    class Bean
     {
     }
 }
---
+++ Pacman/Pacman/Pacman/Metier/Pac.cs
@@ -0,0 +1,46 @@
+﻿using Pacman.IA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pacman.Metier
+{
+    class Pac
+    {
+        private Coord _pos;
+        private ObjetAnime _objAnime;
+        private string _direction;
+
+        public Coord Pos
+        {
+            get { return _pos; }
+            set { _pos = value; }
+        }
+
+        public ObjetAnime ObjAnime
+        {
+            get { return _objAnime; }
+            set { _objAnime = value; }
+        }
+
+        public string Direction
+        {
+            get { return _direction; }
+            set { _direction = value; }
+        }
+
+        public Pac(Coord pos, ObjetAnime objAnime, string direction)
+        {
+            _pos = pos;
+            _objAnime = objAnime;
+            _direction = direction;
+        }
+        public Pac(Coord pos, string direction)
+        {
+            _pos = pos;
+            _direction = direction;
+        }
+
+    }
+}
